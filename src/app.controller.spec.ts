8afcb4b3292ed070ef40af0c9efcd3609a9e1783
Hady Hossam <hady.badr94@gmail.com>
2021-03-15 22:34:27 +0200

    update

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello Hady!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHello()).toBe('Hello Hady!');
     });
   });
 });
