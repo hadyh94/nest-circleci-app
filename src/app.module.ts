0e0f9dbfa87d3a9574f173d69e1f575fddd97ecd
Hady Hossam <hady.badr94@gmail.com>
2021-03-18 17:26:19 +0200

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { DatabaseModule } from './core/database/database.module';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { PostsModule } from './modules/posts/posts.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    UsersModule,
+    AuthModule,
+    PostsModule,
+    ClientsModule.register([
+      {
+        name: 'MATH_SERVICE',
+        transport: Transport.REDIS,
+        options: {
+          url: 'redis://localhost:6379',
+        }
+      },
+    ])],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config.service.ts
@@ -0,0 +1,67 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+require('dotenv').config();
+
+class ConfigService {
+
+  constructor(private env: { [k: string]: string | undefined }) { }
+
+  private getValue(key: string, throwOnMissing = true): string {
+    const value = this.env[key];
+    if (!value && throwOnMissing) {
+      throw new Error(`config error - missing env.${key}`);
+    }
+
+    return value;
+  }
+
+  public ensureValues(keys: string[]) {
+    keys.forEach(k => this.getValue(k, true));
+    return this;
+  }
+
+  public getPort() {
+    return this.getValue('PORT', true);
+  }
+
+  public isProduction() {
+    const mode = this.getValue('MODE', false);
+    return mode != 'DEV';
+  }
+
+  public getTypeOrmConfig(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+
+      host: this.getValue('127.0.0.1'),
+      port: parseInt(this.getValue('5432')),
+      username: this.getValue('postgres'),
+      password: this.getValue('postadmin'),
+      database: this.getValue('myDatabase'),
+
+      entities: ['**/*.entity{.ts,.js}'],
+
+      migrationsTableName: 'migration',
+
+      migrations: ['src/migration/*.ts'],
+
+      cli: {
+        migrationsDir: 'src/migration',
+      },
+
+      ssl: this.isProduction(),
+    };
+  }
+
+}
+
+const configService = new ConfigService(process.env)
+  .ensureValues([
+    '127.0.0.1',
+    '5432',
+    'postgres',
+    'postadmin',
+    'myDatabase'
+  ]);
+
+export { configService };
